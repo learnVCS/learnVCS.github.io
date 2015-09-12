c1c40f2b4db636a72119450d00dffdd601eb719a
Stephanie Jurgiel <snj6366@rit.edu>
2015-09-11 23:14:20 -0400

    switched graphmodal to mobile first, added new fade/size transition, additional styling on graphmodal text

--- app/src/react-components/MessageView.js
+++ app/src/react-components/MessageView.js
@@ -2,16 +2,29 @@ var React = require('react');
 
 var MessageView = React.createClass({
 	render: function () {
-		var messageClassName = "text-wrapper"
+		var messageClassName = "graphModal"
+		var headerClassName = "graphModal__header"
+		var pClassName = "graphModal__p"
+		var authorClassName = "graphModal__p graphModal__p__author"
+		console.log("props: "+ this.props)
 		if (this.props.active) {
-			messageClassName += ' active';
+			messageClassName += ' graphModal_active';
+
 		}
 		return (
 			<div className={messageClassName}>
-				<p>
+				<circle> </circle>
+				<h2 className={headerClassName}> First Commit </h2>
+				<p className={authorClassName}> 
+					{this.props.commit ? this.props.commit.author.name : 'no author'}
+				</p>
+				<p className={pClassName}>
 					{this.props.commit ? this.props.commit.message : 'none selected'}
 				</p>
+				<h3 className={headerClassName}> New </h3>
+				<h3 className={headerClassName}> Changed </h3>
 			</div>
+
 		);
 	}
 });
