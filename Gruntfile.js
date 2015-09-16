c8289b22d4c99b63742b36f8841396dfb1382596
Ben W <reccanti@gmail.com>
2015-09-15 22:31:40 -0400

    Added Web Fonts
    Added the web fonts needed for the page. Placed them in media/fonts. Included them in the sass files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
         	main: {
                 files: [
                     {expand: true, src: ['app/**/*.html', '*.md'], dest: 'build/', flatten: true, filter: 'isFile'},
-                    {expand: true, src: ['app/media/*'], dest: 'build/media/', flatten: true, filter: 'isFile'},
+                    {expand: true, cwd: 'app/media', src: ['**/*'], dest: 'build/media/', filter: 'isFile'},
                     {expand: true, src: ['app/res/*'], dest: 'build/res/', filter: 'isFile'}
                 ]
             }
