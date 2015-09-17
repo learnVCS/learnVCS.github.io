14be58e6fef801a07ce776fb02e312d77f21da75
Stephanie Jurgiel <snj6366@rit.edu>
2015-09-17 00:58:46 -0400

    added icon media, graphhelper.js, and graph.scss navigation.scss

---
+++ app/src/helpers/GraphHelper.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	handleClick();
+});
+
+function handleClick() {
+	
+	$(".searchIcon").on("click", function(e){
+		$(".searchIcon").toggleClass("searchIcon_active");
+		$(".searchModal").toggleClass("searchModal_active");
+	});
+}
