64e0a8c7c4111fa2ffbbca5653d9fd0b33ea12cf
Stephanie Jurgiel <snj6366@rit.edu>
2015-09-27 16:55:20 -0400

    added slide functionality to mobile menu

--- app/src/helpers/GraphHelper.js
+++ app/src/helpers/GraphHelper.js
@@ -1,11 +1,16 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
 	handleClick();
 });
 
 function handleClick() {
-	
+	console.log("listening for clicks");
 	$(".searchIcon").on("click", function(e){
 		$(".searchIcon").toggleClass("searchIcon_active");
 		$(".searchModal").toggleClass("searchModal_active");
 	});
-}*/
+
+	$(".navigation__icon").on("click", function(e){
+		console.log("clicked");
+		$(".navigation__list").toggleClass("navigation__list_active");
+	});
+}
