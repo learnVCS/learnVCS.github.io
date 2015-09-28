19fe9c9a2299af7279199308c0dc13eb0692c096
Stephanie Jurgiel <snj6366@rit.edu>
2015-09-27 20:01:41 -0400

    testing z index for mobile menu sliding

--- app/src/helpers/GraphHelper.js
+++ app/src/helpers/GraphHelper.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	handleClick();
+	addListeners();
 });
 
 function handleClick() {
-	console.log("listening for clicks");
 	$(".searchIcon").on("click", function(e){
 		$(".searchIcon").toggleClass("searchIcon_active");
 		$(".searchModal").toggleClass("searchModal_active");
@@ -11,7 +11,19 @@ function handleClick() {
 
 	$(".navigation__icon").on("click", function(e){
 		console.log("clicked");
+		$(".navigation__list").css("zIndex", -120);
 		$(".navigation__list").toggleClass("navigation__list_active");
 		$(".navigation__icon").toggleClass("navigation__icon_active");
 	});
+}
+
+function addListeners() {
+	var element = document.getElementById("menu");
+	if($(".navigation__list_active")) {
+		element.addEventListener("transitionend", bringToFront, false);
+	}
+}
+
+function bringToFront() {
+  $(".navigation__list_active").css("zIndex", 100);
 }
