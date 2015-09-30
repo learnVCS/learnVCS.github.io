7e17c2058843d2a7527e6138b60876dd513c8012
Stephanie Jurgiel <snj6366@rit.edu>
2015-09-30 02:14:40 -0400

    added close functionality to searchModal issue #16

--- app/src/helpers/GraphHelper.js
+++ app/src/helpers/GraphHelper.js
@@ -1,29 +1,30 @@
 $(document).ready(function(){
 	handleClick();
-	addListeners();
 });
 
 function handleClick() {
+	/* Toggle the search modal */
 	$(".searchIcon").on("click", function(e){
 		$(".searchIcon").toggleClass("searchIcon_active");
 		$(".searchModal").toggleClass("searchModal_active");
 	});
 
+	/* Toggle the mobile navigation menu */
 	$(".navigation__icon").on("click", function(e){
-		console.log("clicked");
-		//$(".navigation__list").css("zIndex", -120);
 		$(".navigation__list").toggleClass("navigation__list_active");
 		$(".navigation__icon").toggleClass("navigation__icon_active");
 	});
-}
 
-function addListeners() {
-	var element = document.getElementById("menu");
-	if($(".navigation__list_active")) {
-		element.addEventListener("transitionend", bringToFront, false);
-	}
-}
+	
+	$(document).click(function(event) { 
 
-function bringToFront() {
-  //$(".navigation__list_active").css("zIndex", 100);
+		/* close search modal when clicking off of the element */
+    if($(event.target).parents(".searchModal").length == 0
+    	&& !$(event.target).hasClass("searchIcon")
+    	&& !$(event.target).hasClass("searchModal")) {
+
+    	$(".searchModal").removeClass("searchModal_active");
+    	$(".searchIcon").removeClass("searchIcon_active");
+    }     
+	})
 }
