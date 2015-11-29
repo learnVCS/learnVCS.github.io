94c158efd1e73508d40c81706e71167e0a897eda
Aaron Sky <aaronsky@skyaaron.com>
2015-11-28 19:36:08 -0500

    Updated 404.html and enhanced error responses

--- app/src/helpers/ErrorHelper.js
+++ app/src/helpers/ErrorHelper.js
@@ -1,21 +1,19 @@
-var blanketResponse = 'Something terrible happened';
+var errors = {
+	default: 'Something terrible happened',
+	400: 'Bad data. Contact an admin',
+	401: 'Invalid credentials',
+	403: 'Rate limit exceeded',
+	404: 'Repository not found',
+};
 
 var ErrorHelper = {
 	appropriateResponse: function (errorCode) {
-		switch(errorCode)
-		{
-			case 404:
-			return 'Repo could not be found';
-			case 401:
-			return 'Access not authorized';
-			default:
-			return blanketResponse;
-		}
+		return errors[errorCode] || errors.default
 	},
 	parse: function (error) {
 		if (error.request.status)
 			return this.appropriateResponse(error.request.status);
-		return blanketResponse;
+		return errors.default;
 	}
 }
 
