917f8e8b2a3ce2bfce34e4ecd5ff282c62836ae3
Aaron Sky <aaronsky@skyaaron.com>
2015-12-04 11:27:50 -0500

    Added extra Grunt steps for build and dev
    * Added sass task to minify CSS in production and ignore sourcemap
    * Added clean task to remove bundle file after minification
    * Added env task to handle switching the NODE_ENV for React
    * Added replace task to automate replacing source targets in index.html
    * Organized task lists in Gruntfile.js
    * Added additional tags to package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,109 +1,159 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        shell: {
-        },
-        clean: {
-            build: ['build'],
-            sassCache: ['.sass-cache']
-        },
-        copy: {
-        	main: {
-                files: [
-                {expand: true, src: ['app/**/*.html', '*.md'], dest: 'build/', flatten: true, filter: 'isFile'},
-                {expand: true, src: ['app/favicon.*'], dest: 'build/', flatten: true, filter: 'isFile'},
-                {expand: true, cwd: 'app/media', src: ['**/*'], dest: 'build/media/', filter: 'isFile'}
-                ]
-            }
-        },
-        watch: {
-            css: {
-                files: [
-                'app/styles/**/*.scss',
-                'app/styles/**/*.sass'
-                ],
-                tasks: ['sass'],
-                options: {
-                    livereload: true
-                }
-            },
-            js: {
-                files: [
-                'app/**/*.js',
-                'Gruntfile.js'
-                ],
-                tasks: ['eslint', 'browserify'],
-                options: {
-                    livereload: true
-                }
-            },
-            other: {
-                files: [
-                'app/**/*.html',
-                './**/*.md',
-                'app/media/*'
-                ],
-                tasks: ['copy'],
-                options: {
-                    livereload: true
-                }
-            }
-        },
-        sass: {
-            dist: {
-                options: {
-                    style: 'expanded'
-                },
-                files: {
-                    'build/main.css': 'app/styles/main.scss'
-                }
-            }
-        },
-        eslint: {
-            options: {
-                silent: true,
-                configFile: 'config/eslintConfig.json'
-            },
-            src: ['Gruntfile.js', 'app/**/*.js']
-        },
-        browserify: {
-            dist: {
-                src: ['app/**/*.js'],
-                dest: 'build/bundle.js',
-                options: {
-                    transform: ['reactify'],
-                    debug: true
-                }
-            },
-        },
-        connect: {
-            livereload: {
-                options: {
-                    open: true,
-                    base: 'build'
-                }
-            }
-        },
-        uglify: {
-            build: {
-                files: {
-                    'build/bundle.min.js': ['build/bundle.js']
-                }
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    shell: {},
+    env: {
+      dev: {
+        NODE_ENV: 'development',
+      },
+      build: {
+        NODE_ENV: 'production'
+      }
+    },
+    clean: {
+      build: ['build'],
+      sassCache: ['.sass-cache'],
+      bundle: ['build/bundle.js'],
+      bundleMin: ['build/bundle.min.js']
+    },
+    copy: {
+      main: {
+        files: [{
+          expand: true,
+          src: ['app/**/*.html', '*.md'],
+          dest: 'build/',
+          flatten: true,
+          filter: 'isFile'
+        }, {
+          expand: true,
+          src: ['app/favicon.*'],
+          dest: 'build/',
+          flatten: true,
+          filter: 'isFile'
+        }, {
+          expand: true,
+          cwd: 'app/media',
+          src: ['**/*'],
+          dest: 'build/media/',
+          filter: 'isFile'
+        }]
+      }
+    },
+    replace: {
+      build: {
+        src: ['build/index.html'],
+        overwrite: true,
+        replacements: [{
+          from: 'main.css',
+          to: 'main.min.css'
+        }, {
+          from: 'bundle.js',
+          to: 'bundle.min.js'
+        }]
+      }
+    },
+    watch: {
+      css: {
+        files: [
+          'app/styles/**/*.scss',
+          'app/styles/**/*.sass'
+        ],
+        tasks: ['sass'],
+        options: {
+          livereload: true
         }
-    });
+      },
+      js: {
+        files: [
+          'app/**/*.js',
+          'Gruntfile.js'
+        ],
+        tasks: ['eslint', 'browserify'],
+        options: {
+          livereload: true
+        }
+      },
+      other: {
+        files: [
+          'app/**/*.html',
+          './**/*.md',
+          'app/media/*'
+        ],
+        tasks: ['copy'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'build/main.css': 'app/styles/main.scss'
+        }
+      },
+      build: {
+        options: {
+          sourcemap: 'none',
+          style: 'compressed'
+        },
+        files: {
+          'build/main.min.css': 'app/styles/main.scss'
+        }
+      }
+    },
+    eslint: {
+      options: {
+        silent: true,
+        configFile: 'config/eslintConfig.json'
+      },
+      src: ['Gruntfile.js', 'app/**/*.js']
+    },
+    browserify: {
+      dist: {
+        src: ['app/**/*.js'],
+        dest: 'build/bundle.js',
+        options: {
+          transform: ['reactify'],
+          debug: true
+        }
+      },
+    },
+    connect: {
+      livereload: {
+        options: {
+          open: true,
+          base: 'build'
+        }
+      }
+    },
+    uglify: {
+      build: {
+        files: {
+          'build/bundle.min.js': ['build/bundle.js']
+        }
+      }
+    }
+  });
 
-grunt.loadNpmTasks('grunt-shell');
-grunt.loadNpmTasks('grunt-contrib-clean');
-grunt.loadNpmTasks('grunt-contrib-copy');
-grunt.loadNpmTasks('grunt-contrib-sass');
-grunt.loadNpmTasks('gruntify-eslint');
-grunt.loadNpmTasks('grunt-browserify');
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-contrib-connect');
-grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('gruntify-eslint');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-grunt.registerTask('default', ['clean', 'copy', 'sass', 'eslint', 'browserify', 'connect', 'watch']);
-grunt.registerTask('ci', ['clean', 'copy', 'sass', 'eslint', 'browserify', 'uglify'])
-grunt.registerTask('build', ['clean', 'copy', 'sass', 'eslint', 'browserify', 'uglify']);
+  var devTaskList = ['env:dev', 'clean', 'copy', 'sass:dev', 'eslint', 'browserify', 'connect', 'watch'];
+  var prodTaskList = ['env:build', 'clean', 'copy', 'sass:build', 'eslint', 'browserify', 'uglify', 'clean:bundle', 'replace'];
+
+  grunt.registerTask('default', devTaskList);
+  grunt.registerTask('ci', prodTaskList);
+  grunt.registerTask('build', prodTaskList);
 };
