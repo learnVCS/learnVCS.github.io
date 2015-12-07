a6bbab7a836e0512b7b755051bdb14d88617f9eb
Aaron Sky <aaronsky@skyaaron.com>
2015-12-07 00:20:58 -0500

    Made changes to improve site performance according to recommendations

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,22 +20,26 @@ module.exports = function(grunt) {
       main: {
         files: [{
           expand: true,
-          src: ['app/**/*.html', '*.md'],
+          src: ['app/**/*.html', '*.md', 'humans.txt', 'robots.txt', 'sitemap.xml'],
           dest: 'build/',
           flatten: true,
           filter: 'isFile'
         }, {
           expand: true,
-          src: ['app/favicon.*'],
+          cwd: 'app/',
+          src: ['media/fonts/**/*'],
           dest: 'build/',
-          flatten: true,
           filter: 'isFile'
-        }, {
+        }]
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
           expand: true,
-          cwd: 'app/media',
-          src: ['**/*'],
-          dest: 'build/media/',
-          filter: 'isFile'
+          cwd: 'app/',
+          src: ['favicon.*', 'media/**/*.{png,jpg,svg}'],
+          dest: 'build/'
         }]
       }
     },
@@ -58,28 +62,31 @@ module.exports = function(grunt) {
           'app/styles/**/*.scss',
           'app/styles/**/*.sass'
         ],
-        tasks: ['sass'],
-        options: {
-          livereload: true
-        }
+        tasks: ['newer:sass']
       },
       js: {
         files: [
           'app/**/*.js',
           'Gruntfile.js'
         ],
-        tasks: ['eslint', 'browserify'],
-        options: {
-          livereload: true
-        }
+        tasks: ['newer:eslint', 'newer:browserify']
+      },
+      media: {
+        files: ['app/media/**/*', 'app/favicon.*'],
+        tasks: ['newer:imagemin']
       },
       other: {
         files: [
           'app/**/*.html',
           './**/*.md',
-          'app/media/*'
+          'humans.txt',
+          'robots.txt',
+          'sitemap.xml'
         ],
-        tasks: ['copy'],
+        tasks: ['newer:copy']
+      },
+      livereload: {
+        files: ['build/**/*'],
         options: {
           livereload: true
         }
@@ -138,10 +145,12 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-newer');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-env');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-text-replace');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('gruntify-eslint');
@@ -150,8 +159,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  var devTaskList = ['env:dev', 'clean', 'copy', 'sass:dev', 'eslint', 'browserify', 'connect', 'watch'];
-  var prodTaskList = ['env:build', 'clean', 'copy', 'sass:build', 'eslint', 'browserify', 'uglify', 'clean:bundle', 'replace'];
+  var devTaskList = ['env:dev', 'clean', 'imagemin', 'copy', 'sass:dev', 'eslint', 'browserify', 'connect', 'watch'];
+  var prodTaskList = ['env:build', 'clean', 'imagemin', 'copy', 'sass:build', 'eslint', 'browserify', 'uglify', 'clean:bundle', 'replace'];
 
   grunt.registerTask('default', devTaskList);
   grunt.registerTask('ci', prodTaskList);
