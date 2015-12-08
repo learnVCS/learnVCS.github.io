fa210cf92625a747c19f1f09840d0b1e296f1762
Ben W <reccanti@gmail.com>
2015-12-07 19:43:21 -0500

    CSS Transition for Graph Node
    When the user clicks on the node, the graph node should expand. Right now the
    node expands and has the expected appearance, but it doesn't perform the
    appropriate transition.

--- app/src/react-components/MessageView.js
+++ app/src/react-components/MessageView.js
@@ -2,6 +2,24 @@ var React = require('react');
 
 var MessageView = React.createClass({
 	
+	getInitialState: function() {
+		return { badgeExpand: "" };
+	},
+
+	componentDidMount: function() {
+
+		// var _this = this;
+		// window.requestAnimationFrame(function() {
+		// 	console.log("bluh");
+	 //    	var node = _this.getDOMNode();
+	 //    	if (node !== undefined) {
+	 //    	  //and scroll them!
+	 //    	  node.classList.add("graphModal__circle--active");
+	 //    	}
+		// });
+		this.setState({badgeExpand: " graphModal__circle--active"});
+	},
+
 	render: function () {
 		var commit = this.props.commit || false;
 		var message = commit ? commit.message : '';
@@ -9,8 +27,22 @@ var MessageView = React.createClass({
 		return (
 			<div className={'graphModal' + (this.props.active ? ' graphModal_active' : '')}>
 				<svg className="graphModal__svg">
-					<circle cx="0" cy="96" r="10" fill="white" className="graphModal__circle"></circle>
-					<circle cx="0" cy="96" r="6" fill={this.props.color || 'red'} className="graphModal__circle"></circle>
+					<g>
+						<circle cx="0"
+								cy="96" 
+								r="10" 
+								fill="white"
+								strokeWidth="4"
+								stroke="black" 
+								className="graphModal__circle graphModal__circle--active">
+						</circle>
+						<circle cx="0" 
+								cy="96" 
+								r="10" 
+								fill={this.props.color || 'red'} 
+								className="graphModal__circle">
+						</circle>
+					</g>
 				</svg>
 				<div className="graphModal__content">
 				<h2 className='graphModal__header'>{firstLine}</h2>
